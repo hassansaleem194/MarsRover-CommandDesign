7954975057f9e0326aa10d951067fa2f1d9e491b
Tim Turvey <tturvey@hotmail.co.uk>
2022-09-14 10:25:05 +0100

    Green: turn right once passes

---
+++ src/main/java/org/codurance/MarsRover.java
@@ -0,0 +1,8 @@
+package org.codurance;
+
+public class MarsRover {
+
+    public String execute(String r) {
+        return "0:0:E";
+    }
+}
--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -1,2 +1,13 @@
+import org.codurance.MarsRover;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.*;
+
 public class MarsRoverShould {
+
+    @Test
+    public void
+    turn_right_when_passed_one_right() {
+        assertEquals( "0:0:E", new MarsRover().execute("R"));
+    }
 }
